6eb668afd9735051161842be614b615c336dc06b
Rishi Kumar <chawda.rishii@gmail.com>
2018-05-17 10:06:56 +0530

    lrvc

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -9,24 +9,24 @@ export class CockpitComponent implements OnInit {
 
   @Output() server_created = new EventEmitter<{server_name: string, server_content: string}>();
   @Output('bp_created') blueprint_created = new EventEmitter<{server_name: string, server_content: string}>();
-  new_servername = '';
   new_servercontent = '';
   allow_new_servers = false;
+  @ViewChild('server_content_input') server_content_input: ElementRef;
 
   constructor() { }
 
   ngOnInit() {
-    setTimeout( ()=>{
+    setTimeout( () => {
       this.allow_new_servers = true;
     }, 2000 );
   }
 
-  on_create_server() {
-    this.server_created.emit({server_name: this.new_servername, server_content: this.new_servercontent});
+  on_create_server(server_name_input: HTMLInputElement) {
+    this.server_created.emit({server_name: server_name_input.value, server_content: this.server_content_input.nativeElement.value});
   }
 
-  on_create_blueprint() {
-    this.blueprint_created.emit({server_name: this.new_servername, server_content: this.new_servercontent});
+  on_create_blueprint(server_name_input: HTMLInputElement) {
+    this.blueprint_created.emit({server_name: server_name_input.value, server_content: this.server_content_input.nativeElement.value});
   }
 
 }
