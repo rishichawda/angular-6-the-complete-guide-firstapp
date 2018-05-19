1923f3225dfaaaa58b747eb22f66cd8c24f398a9
Rishi Kumar <chawda.rishii@gmail.com>
2018-05-19 11:48:57 +0530

    hb

--- src/app/server-hover/server-hover.directive.ts
+++ src/app/server-hover/server-hover.directive.ts
@@ -1,21 +1,31 @@
-import { OnInit, Directive, ElementRef, Renderer2, HostBinding, HostListener } from "@angular/core";
+import { OnInit,
+     Directive,
+     ElementRef,
+     Renderer2,
+     HostBinding,
+     HostListener
+    } from '@angular/core';
 
 @Directive({
     selector: '[appServerHoverDirective]'
 })
 export class ServerHoverDirective implements OnInit {
 
-    constructor(private elRef: ElementRef, private renderer: Renderer2) {}
+    @HostBinding('style.backgroundColor') backgroundcolor = 'transparent';
 
-    ngOnInit(){
+    constructor( private elRef: ElementRef, private renderer: Renderer2) {}
+
+    ngOnInit() {
         // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'rgba(0,0,0,0.2)');
     }
-    
-    @HostListener('mouseenter') mousehover(){
-        this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'rgba(0,0,0,0.2)');
+
+    @HostListener('mouseenter') mousehover() {
+        // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'rgba(0,0,0,0.2)');
+        this.backgroundcolor = 'rgba(0,0,0,0.2)';
     }
-    
-    @HostListener('mouseleave') mouseleave(){
-        this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'rgba(0,0,0,0)');
+
+    @HostListener('mouseleave') mouseleave() {
+        // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'rgba(0,0,0,0)');
+        this.backgroundcolor = 'transparent';
     }
 }
