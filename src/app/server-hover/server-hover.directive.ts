396f0f8beca5da0f10e89f835b7e98ccb76a28ed
Rishi Kumar <chawda.rishii@gmail.com>
2018-05-19 11:41:33 +0530

    hl

--- src/app/server-hover/server-hover.directive.ts
+++ src/app/server-hover/server-hover.directive.ts
@@ -1,12 +1,21 @@
-import { OnInit, Directive, ElementRef, Renderer2 } from "@angular/core";
+import { OnInit, Directive, ElementRef, Renderer2, HostBinding, HostListener } from "@angular/core";
 
 @Directive({
     selector: '[appServerHoverDirective]'
 })
 export class ServerHoverDirective implements OnInit {
+
     constructor(private elRef: ElementRef, private renderer: Renderer2) {}
 
     ngOnInit(){
+        // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'rgba(0,0,0,0.2)');
+    }
+    
+    @HostListener('mouseenter') mousehover(){
         this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'rgba(0,0,0,0.2)');
+    }
+    
+    @HostListener('mouseleave') mouseleave(){
+        this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'rgba(0,0,0,0)');
     }
 }
