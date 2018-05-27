fc67bb2b3bdf545f85fd45ec103884205ba9ee30
Rishi Kumar <chawda.rishii@gmail.com>
2018-05-27 19:29:17 +0530

    update project structure for routing section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,49 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { AccountService } from './shared/account-service.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [AccountService]
+  styleUrls: ['./app.component.css']
 })
-
-export class AppComponent implements OnInit {
-
-  accounts: {name: string, status: string}[] = [];
-
-  constructor(private accounts_service: AccountService) {}
-
-  ngOnInit() {
-    this.accounts = this.accounts_service.accounts;
-  }
-
-  // server_elements = [
-  //   { type: 'server', name: 'test server', content: 'this is a test server' },
-  //   { type: 'server', name: 'test server', content: 'this is a test server' }
-  // ];
-
-  // on_server_added(server_data: {server_name: string, server_content: string}) {
-  //   this.server_elements.push({
-  //     type: 'server',
-  //     name: server_data.server_name,
-  //     content: server_data.server_content
-  //   });
-  // }
-
-  // on_blueprint_added(server_data: {server_name: string, server_content: string}) {
-  //   this.server_elements.push({
-  //     type: 'blueprint',
-  //     name: server_data.server_name,
-  //     content: server_data.server_content
-  //   });
-  // }
-
-  // on_change_first() {
-  //   this.server_elements[0].name = 'this element name was changed!';
-  // }
-
-  // on_destroy_first(){
-  //   this.server_elements.splice(0,1);
-  // }
+export class AppComponent {
 }
--- src/app/shared/logging.service.ts
+++
@@ -1,5 +0,0 @@
-export class LoggingService {
-    logger(status: string) {
-        console.log('Account status was changed to ' + status);
-    }
-}
