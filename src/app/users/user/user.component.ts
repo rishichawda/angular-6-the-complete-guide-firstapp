2c3a268622e8ca70a3f63d515a883d7075c83d89
Rishi Kumar <chawda.rishii@gmail.com>
2018-05-30 18:04:15 +0530

    parameter subscriptions

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   user: {id: number, name: string};
+  params_subscription: Subscription;
 
   constructor(private route: ActivatedRoute) { }
 
@@ -16,6 +18,16 @@ export class UserComponent implements OnInit {
       id: this.route.snapshot.params['id'],
       name: this.route.snapshot.params['name']
     };
+
+    this.route.params.subscribe(
+      (updated_params: Params) => { 
+        this.user.id = updated_params['id'];
+        this.user.name = updated_params['name'];
+       }
+    );
   }
 
+  ngOnDestroy() {
+    this.params_subscription.unsubscribe();
+  }
 }
